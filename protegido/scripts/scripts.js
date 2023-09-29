990e554636876f6d4f7732a3b3e88bd18eb3f29b
Andre-Alvarenga <129390193+Andre-Alvarenga@users.noreply.github.com>
2023-09-29 09:27:12 -0300

    feat: mensagem

--- protegido/scripts/scripts.js
+++ protegido/scripts/scripts.js
@@ -3,6 +3,17 @@ window.onload = () =>{
         obterClientes();
 }
 
+function mostrarMensagem(mensagem,tipo){
+    let divMensagem = document.getElementById('Mensagem');
+    divMensagem.innerHTML = `<div class="alert alert-${tipo}" role="alert">
+                                   ${mensagem}
+                            </div>`;
+    setTimeout(()=>{
+        divMensagem.innerHTML = '';
+    },5000);
+
+}
+
 function obterClientes(){
     fetch('https://129.146.68.51/aluno1-ppiadsead/clientes', {method:'GET'})
     .then((resposta)=>{
@@ -16,13 +27,47 @@ function obterClientes(){
     .then((listaClientes)=>{
         mostrarClientes(listaClientes);
     })
+    .catch((erro)=>{
+        mostrarMensagem('Não foi possível obter os clientes do backend. Erro: ' + erro.message,'danger');
+    })
 };
 
+function cadastrarCliente(cliente){
+    fetch('https://129.146.68.51/aluno1-ppiadsead/clientes',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(cliente)
+    }).then((resposta)=>{
+        if (resposta.status === 200){
+            return json();
+        }
+        else{
+            return {
+                status:false,
+                mensagem:'Não foi possivel enviar o cliente para o backend.'
+            };
+        }
+    }).then((respostaBackEnd)=>{
+        if(respostaBackEnd.status){
+            mostrarMensagem(respostaBackEnd.mensagem,'success');
+        }
+        else{
+            mostrarMensagem(respostaBackEnd.mensagem,'danger');
+        }
+    }).catch((erro)=>{
+        mostrarMensagem(erro.message,'danger');
+    });
+}
+
+
 function mostrarClientes(listaClientes){
-    let elementoDivTabela = document.getElementById('espacoTabela');
+    let elementoDivTabela = document.getElementById("espacoTabela");
     elementoDivTabela.innerHTML = '';
     if (listaClientes.lenght > 0){;
-        let tabela = document.createElement('table');
+        let tabela = document.createElement('table'); 
+        tabela.className ='table table-striped table-hover'
         let cabecalhoTabela = document.createElement('thead');
         let corpoTabela = document.createElement('tbody');
 
@@ -53,8 +98,7 @@ function mostrarClientes(listaClientes){
                                 <td>${cliente.endereço}</td>
                                 <td>${cliente.curso}</td>
                                 <td>${cliente.periodo}</td>
-                                <td>${cliente.mensagem}</td>
-                                <td></td>`;
+                                <td>${cliente.mensagem}</td>`;
         corpoTabela.appendChild(linhatabela);
 
        }
